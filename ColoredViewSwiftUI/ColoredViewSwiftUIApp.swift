8a8e9328f7a62d4e89aae8f67cd071dbc35bd203
GubinYuriy <iosdevelop.2022@gmail.com>
2022-12-13 17:02:24 +0500

    Initial Commit

---
+++ ColoredViewSwiftUI/ColoredViewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColoredViewSwiftUIApp.swift
+//  ColoredViewSwiftUI
+//
+//  Created by Юрий Губин on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColoredViewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
